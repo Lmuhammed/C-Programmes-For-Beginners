1acc2f50eec3b9760fd06f441e94caf9f16fe344
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-06-07 13:17:05 +0100

    Strings/StringLength.c added

---
+++ Strings/StringLength.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void){
+    
+    char str[20];
+    printf("Enter a string :");
+    scanf("%[^\n]",str);
+    int i=0,strCOunt=0
+    for (i=0 ; str[i]!='\0' ; i++);
+    strCOunt++;
+    printf("The String lenght %d\n",strCOunt);
+    return 0;
+}   
