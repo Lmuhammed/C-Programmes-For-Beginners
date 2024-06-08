214ef84c7609112ce3eb853bae0b8b838d74375d
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-06-08 01:06:25 +0100

    Print/printfReturn.c added

---
+++ Print/printfReturn.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    //printf return number of printed chars
+    if(printf("Hello")){
+     printf("World \n");
+    }else{
+    printf("YOu will not see me :D\n");
+    }
+     return 0;
+}   
