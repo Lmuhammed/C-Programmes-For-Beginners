6186ced95622f5839a83e7939d7d6ec10f935bb8
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-06-09 03:23:53 +0100

    other/register.c

---
+++ other/register.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    register int i; 
+
+    for (i = 0; i < 10; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
+/*
+ In C, the register keyword is used to suggest to the compiler that a particular variable should be stored in a CPU register for faster access. However, it's important to note that the register keyword is just a hint to the compiler, and the compiler may choose to ignore it.
+*/ 
