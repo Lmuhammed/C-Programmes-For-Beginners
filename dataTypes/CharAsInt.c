baa3bee2cb7ff269d11c6dd74644d840e051feb8
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-06-11 20:24:36 +0100

    dataTypes/ & CharAsInt.c added

---
+++ dataTypes/CharAsInt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char number = 22; // Storing the number 22 in a char variable
+
+    if (number == 22) {
+        printf("The number is equal to 22.\n");
+    } else {
+        printf("The number is not equal to 22.\n");
+    }
+
+    return 0;
+}
