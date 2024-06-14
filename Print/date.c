96745c154c4c07addd9e8047ed9fffda3fe693d0
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-06-14 02:11:45 +0100

    Print/date.c moved to Projects/Usefull Macros/TimeDate.c

--- Print/date.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(void){
-    printf("Today DATE :%s \n ",__DATE__);
-     return 0;
-}   
---
+++ Projects/Usefull Macros/TimeDate.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(void){
+
+    printf("Time    :%s\n",__TIME__);
+    printf("Date    :%s\n",__DATE__);
+     return 0;
+}   
