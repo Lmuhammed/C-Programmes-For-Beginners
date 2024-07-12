cd4b60867b240965fc96c9d888173868a2f383ab
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-07-12 19:06:49 +0100

    move limith.c to /std libs/ & differentWaysDeclareStruct.c added

---
+++ struct/differentWaysDeclareStruct.c
@@ -0,0 +1,27 @@
+
+/*
+
+method 1
+ struct Person {
+    char name[50];
+    int age;
+};
+
+*/
+
+/*
+
+method 2 : using typedef to create an alias for the struct
+
+typedef struct {
+    char name[50];
+    int age;
+} Person;
+
+*/
+
+int main()
+{
+
+    return 0;
+}
