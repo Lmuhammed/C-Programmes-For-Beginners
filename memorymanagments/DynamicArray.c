c5a06c442e2d572f5ac8637e6d3e95ee9229b5e2
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2024-08-17 17:42:10 +0100

    Reorganized directory structure to enhance clarity and maintainability.

--- memorymanagments/DynamicArray.c
+++ memorymanagments/DynamicArray.c
@@ -4,6 +4,7 @@
 int main(void){
 
     int *a,i=0;
+    //حجز جزء من الذاكرة عبارة مصفوفة  بحجم 3 اعداد صحيحة
     a=malloc(sizeof(int)*3);
     a[0]=1;
     a[1]=2;
@@ -11,6 +12,8 @@ int main(void){
     printf("Print Dynamiclly allocated array\n");
     for(i=0;i<3;i++)
     printf("a[%d] = %d \n ",i,a[i]);
+    //إعادة الذاكرة المحجوزة لنظام التشغيل بعد الإنتهاء من إستخدامها
+    free(a);
     
     return 0;
 }   
--- other/register.c
+++ other/register.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
+    
+    //مفتاح السجل
+    //register keyword
     register int i; 
 
     for (i = 0; i < 10; i++) {
@@ -10,5 +13,7 @@ int main() {
     return 0;
 }
 /*
+ في C، تُستخدم كلمة السجل للإيحاء للمترجم بضرورة تخزين متغير معين في سجل وحدة المعالجة المركزية للوصول إليه بشكل أسرع. ومع ذلك، من المهم ملاحظة أن كلمة مفتاح السجل هي مجرد تلميح للمترجم، وقد يختار المترجم تجاهلها.
+
  In C, the register keyword is used to suggest to the compiler that a particular variable should be stored in a CPU register for faster access. However, it's important to note that the register keyword is just a hint to the compiler, and the compiler may choose to ignore it.
 */ 
