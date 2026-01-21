1877d753f9b6b088a9e55cc14f2ce72e9cf72eec
Lmuhammed <140099085+Lmuhammed@users.noreply.github.com>
2026-01-21 16:40:42 +0100

    strncmp() used in projects/password.c for better security

--- projects/password.c
+++ projects/password.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+#define PWD_LEN 20
 #define MAX_ATTEMPTS 3
 #define PASSWORD "password123"
 
 int main() {
-    char input[20];
+    char input[PWD_LEN];
     int attempts = 0;
-
-    while (attempts < MAX_ATTEMPTS) {
-        printf("Enter the password: ");
+ 
+    do {
+	printf("Enter the password: ");
         scanf("%s", input);
 
-        if (strcmp(input, PASSWORD) == 0) {
+        if (strncmp(input, PASSWORD,PWD_LEN) == 0) {
             printf("Login successful!\n");
             break;
         } else {
             attempts++;
             printf("Incorrect password. Attempts left: %d\n", MAX_ATTEMPTS - attempts);
         }
-    }
 
+    }while(attempts < MAX_ATTEMPTS);
     if (attempts == MAX_ATTEMPTS) {
         printf("Maximum login attempts reached. Login failed.\n");
     }
